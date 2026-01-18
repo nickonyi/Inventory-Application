8cec40d0b421acb826d1c1c802a887d70d1e26cc
nickonyi <nickoluoch1130@gmail.com>
2026-01-18 16:00:30 +0300

    feat: integrate Tailwind CSS and set up error handling; add 404 page and view templates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler.js";
 dotenv.config();
+PORT = process.env.PORT;
 
 const app = express();
 
-app;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/{*splat}", (req, res) => {
+  res.status(404).render("404");
+});
+
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`Listening at port:${PORT}`);
+});
